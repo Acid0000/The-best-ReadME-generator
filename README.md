8d0661339c0f6d9ac19b1fb60f394567dd2cfdf1
Acid0001 <pissboyree@gmail.com>
2024-08-02 00:38:24 -0700

    Completed code

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
-# The best ReadME generator
- This is the last ReadME generator that you'll need!
+# ReadMe Generator
+
+![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+
+## Description
+
+The best readme generator ever
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+download and run it! :)
+
+## Usage
+
+Use it however you see fit
+
+## License
+
+This project is licensed under the [MIT](https://opensource.org/licenses/MIT) license.
+
+## Contributing
+
+Ehhhhh Nah
+
+## Tests
+
+npm test
+
+## Questions
+
+If you have any questions, please reach out:
+
+- GitHub: [Someone](https://github.com/Someone)
+- Email: [someone@gmail.com](mailto:someone@gmail.com)
--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,19 +1,81 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  switch (license) {
+    case 'MIT':
+      return '![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)';
+    case 'GPLv3':
+      return '![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)';
+    case 'Apache 2.0':
+      return '![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)';
+    default:
+      return '';
+  }
+}
 
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case 'MIT':
+      return '[MIT](https://opensource.org/licenses/MIT)';
+    case 'GPLv3':
+      return '[GPLv3](https://www.gnu.org/licenses/gpl-3.0)';
+    case 'Apache 2.0':
+      return '[Apache 2.0](https://opensource.org/licenses/Apache-2.0)';s
+    default:
+      return '';
+  }
+}
 
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === 'None') {
+    return '';
+  } else {
+    return `## License
 
-// TODO: Create a function to generate markdown for README
+This project is licensed under the ${renderLicenseLink(license)} license.`;
+  }
+}
+
 function generateMarkdown(data) {
   return `# ${data.title}
 
+${renderLicenseBadge(data.license)}
+
+## Description
+
+${data.description}
+
+## Table of Contents
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [License](#license)
+- [Contributing](#contributing)
+- [Tests](#tests)
+- [Questions](#questions)
+
+## Installation
+
+${data.installation}
+
+## Usage
+
+${data.usage}
+
+${renderLicenseSection(data.license)}
+
+## Contributing
+
+${data.contributing}
+
+## Tests
+
+${data.tests}
+
+## Questions
+
+If you have any questions, please reach out:
+
+- GitHub: [${data.github}](https://github.com/${data.github})
+- Email: [${data.email}](mailto:${data.email})
 `;
 }
 
